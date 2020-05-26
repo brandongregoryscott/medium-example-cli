f552a56ddb47d35f3c863c548e0c4de5c7939e24
Brandon Scott <bscott@andculture.com>
2020-05-26 16:14:36 -0400

    Added dotnetClean.run() tests

--- phase3/_modules/dotnet-clean.test.js
+++ phase3/_modules/dotnet-clean.test.js
@@ -27,5 +27,46 @@ describe("dotnetClean", () => {
             // Assert
             expect(dotnetPathSpy).toHaveBeenCalled();
         });
+
+        test("when no 'bin' or 'obj' directories exist, it does not call shell.rm", () => {
+            // Arrange
+            const shellFindSpy = jest.spyOn(shell, "find").mockImplementation(() => []);
+            const shellRmSpy = jest.spyOn(shell, "rm");
+
+            // Act
+            dotnetClean.run();
+
+            // Assert
+            expect(shellFindSpy).toHaveBeenCalled();
+            expect(shellRmSpy).not.toHaveBeenCalled();
+        });
+
+        test("when '.git' directory exists, it is not passed to shell.rm", () => {
+            // Arrange
+            const shellFindSpy = jest.spyOn(shell, "find").mockImplementation(() => [".git", "bin", "obj"]);
+            const shellRmSpy = jest.spyOn(shell, "rm");
+
+            // Act
+            dotnetClean.run();
+
+            // Assert
+            expect(shellFindSpy).toHaveBeenCalled();
+            expect(shellRmSpy).toHaveBeenCalledWith("-rf", ["bin"]);
+            expect(shellRmSpy).toHaveBeenCalledWith("-rf", ["obj"]);
+        });
+
+        test("when 'node_modules' directory exists, it is not passed to shell.rm", () => {
+            // Arrange
+            const shellFindSpy = jest.spyOn(shell, "find").mockImplementation(() => ["node_modules", "bin", "obj"]);
+            const shellRmSpy = jest.spyOn(shell, "rm");
+
+            // Act
+            dotnetClean.run();
+
+            // Assert
+            expect(shellFindSpy).toHaveBeenCalled();
+            expect(shellRmSpy).toHaveBeenCalledWith("-rf", ["bin"]);
+            expect(shellRmSpy).toHaveBeenCalledWith("-rf", ["obj"]);
+        });
     });
 });
